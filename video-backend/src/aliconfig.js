becd37866e4040ee8df607ee37bac1cd056ff2e6
RongRongJi <316315867@qq.com>
2021-06-03 00:31:41 +0800

    gitignore

--- video-backend/src/aliconfig.js
+++
@@ -1,4 +0,0 @@
-export default{
-    accessKeyId: 'LTAI5tKGU7NcDfEerksEzNzT',
-    accessKeySecret: '1N5B0f7b8hdL3zFUpKt7GBnqcLHw7X',
-}
--- video-together/src/core/data/config.js
+++
@@ -1,4 +0,0 @@
-export default{
-    appId:"9fu5lw6q",
-    appKey:"778a0d58e63edcacaccbf57231cd47fd"
-}
